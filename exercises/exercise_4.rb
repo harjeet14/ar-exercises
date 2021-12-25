5c2d9eda1ddb269eb9a282f1dcb8ff252c8f3a70
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-12-24 22:22:15 -0500

    add new stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.create(
+  name:"Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+
+)
+@store5 = Store.create(
+  name:"Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+@store6 = Store.create(
+  name:"Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+@mens_stores = Store.where(mens_apparel:true)
+for @store in @mens_stores do
+  puts "Here: #{@store.name} And #{@store.annual_revenue}"
+end
+# @womens_stores = Store.where("womens_apparel = :womens_apparel and annual_revenue < :annual_revenue", womens_apparel:true, annual_revenue:1000000)
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+for @store in @womens_stores do
+  puts "Here: #{@store.name} And #{@store.annual_revenue}"
+end
+# puts "here2 : #{@womens_stores}"
+puts "Total numbers of store #{Store.count}."
