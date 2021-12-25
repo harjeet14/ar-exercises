e4f8862b85a03688d4d7537bd22fb8e973a016ad
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-12-25 10:21:40 -0500

    total and avg revenue of the stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_revenue = Store.average(:annual_revenue)
+puts "Total revenue of all the stores is #{total_revenue}."
+puts "Average revenue of all the stores is #{average_revenue}"
+more_than_million_rev = Store.where("annual_revenue >= 1000000").size
+puts "Stores more than 1M revenue is #{more_than_million_rev}."
